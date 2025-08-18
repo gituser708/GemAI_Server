710903bcffd539f0bf0bb53e59bdc6406952f034
gituser708 <donmania999@gmail.com>
2025-08-18 18:39:52 +0530

    6th commit

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -68,7 +68,7 @@ const userCtrl = {
             res.cookie("token", token, {
                 httpOnly: true,
                 secure: true,
-                sameSite: "strict",
+                sameSite: "none",
                 maxAge: 30 * 24 * 60 * 60 * 1000
             });
 
